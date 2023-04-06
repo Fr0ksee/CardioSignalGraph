1449124b7ada001120aa101e720604f54a57b2f6
Illia Balas <mr.balasillia@gmail.com>
2023-04-07 02:01:48 +0300

    All is done except numericUpDown intagration and limits from the task implementation.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,13 +20,11 @@ namespace CardioSignalGraph
         public SignalWave S = new SignalWave(-0.17, 0.54, 0.02);
         public SignalWave T = new SignalWave(0.2, 0.7, 0.04);
         public SignalWave[] arr = new SignalWave[5];
-
         public Form()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             chart.ChartAreas[0].AxisY.Maximum = 1.2;
@@ -38,29 +37,6 @@ namespace CardioSignalGraph
             Arr_init();
             Chart_Update();
         }
-
-        private void WidthBar1_ValueChanged(object sender, EventArgs e)
-        { 
-            if (!radioButtonT.Checked)
-                WidthBar2.Value = WidthBar1.Value;
-            
-            double temp_value = WidthBar1.Value / 10.0;
-            arr[RadioButtonCheck()].l_width = arr[RadioButtonCheck()].width * temp_value;
-
-            Chart_Update();
-        }
-
-        private void WidthBar2_ValueChanged(object sender, EventArgs e)
-        {
-            if (!radioButtonT.Checked)
-                WidthBar1.Value = WidthBar2.Value;
-
-            double temp_value = WidthBar2.Value / 10.0;
-            arr[RadioButtonCheck()].r_width = arr[RadioButtonCheck()].width * temp_value;
-
-            Chart_Update();
-        }
-
         private void Chart_Update()
         {
             chart.Series[0].Points.Clear();
@@ -70,8 +46,8 @@ namespace CardioSignalGraph
 
             for (int i = 0; i < arr.Length; i++)
             {
-                double a = arr[i].amplitude;
-                double m = arr[i].amp_moment;
+                double a = arr[i].current_amplitude;
+                double m = arr[i].current_moment;
                 
 
                 for (int j = 0; j < yValues.Length; j++)
@@ -87,14 +63,12 @@ namespace CardioSignalGraph
                     yValues[j] += y;
                 }
             }
-
             for (int i = 0; i < yValues.Length; i++)
             {
                 double x = i / 1000.0;
                 chart.Series[0].Points.AddXY(x, yValues[i]);
             }
         }
-
         private int RadioButtonCheck()
         {
             if (radioButtonP.Checked) { return 0; }
@@ -104,7 +78,6 @@ namespace CardioSignalGraph
             else if (radioButtonT.Checked) { return 4; }
             else { return -1; }
         }
-
         public void Arr_init()
         {
             arr[0] = P;
@@ -113,28 +86,66 @@ namespace CardioSignalGraph
             arr[3] = S;
             arr[4] = T;
         }
+        private void AmplitudeBar_ValueChanged(object sender, EventArgs e)
+        {
+            int i = RadioButtonCheck();
+            double temp_value = AmplitudeBar.Value / 10.0;
+            arr[i].current_amplitude = arr[i].default_amplitude * temp_value;
 
+            Chart_Update();
+        }
         private void TimeBar_ValueChanged(object sender, EventArgs e)
         {
+            int i = RadioButtonCheck();
+            double temp_value = (TimeBar.Value - 10.0) / 100.0;
+            if (TimeBar.Value == 10)
+            {
+                arr[i].current_moment = arr[i].default_moment;
+            }
+            else
+            {
+                arr[i].current_moment = arr[i].default_moment + temp_value;
+            }
 
+            Chart_Update();
+        }
+        private void WidthBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (!radioButtonT.Checked)
+                WidthBar2.Value = WidthBar1.Value;
+
+            double temp_value = WidthBar1.Value / 10.0;
+            arr[RadioButtonCheck()].l_width = arr[RadioButtonCheck()].width * temp_value;
+
+            Chart_Update();
+        }
+        private void WidthBar2_ValueChanged(object sender, EventArgs e)
+        {
+            if (!radioButtonT.Checked)
+                WidthBar1.Value = WidthBar2.Value;
+
+            double temp_value = WidthBar2.Value / 10.0;
+            arr[RadioButtonCheck()].r_width = arr[RadioButtonCheck()].width * temp_value;
+
+            Chart_Update();
         }
     }
     public class SignalWave
     {
-        public double amplitude { get; set; }
-        public double  amp_moment { get; set; }
-        public double width { get; set; }
+        public double default_amplitude { get; }
+        public double  default_moment { get; }
+        public double width { get; }
         public double r_width { get; set; }
         public double l_width { get; set; }
-        public double temp_amplitude { get; set; }
-        public double temp_moment { get; set; }
+        public double current_amplitude { get; set; }
+        public double current_moment { get; set; }
 
-        public SignalWave(double amplitude, double amp_moment, double width)
+        public SignalWave(double amplitude, double default_moment, double width)
         {
-            this.amplitude = amplitude;
-            this.temp_amplitude = amplitude;
-            this.amp_moment = amp_moment;
-            this.temp_moment = amp_moment;
+            this.default_amplitude = amplitude;
+            this.current_amplitude = amplitude;
+            this.default_moment = default_moment;
+            this.current_moment = default_moment;
             this.width = width;
             this.r_width = width;
             this.l_width = width;
